7a10015d79db57f04bba0163bb9b53831c499775
Robert Gründler <robert@dubture.com>
2018-05-09 08:46:48 +0200

    ADDED missing config

--- child/src/main/java/child/FileConfig.java
+++ child/src/main/java/child/FileConfig.java
@@ -3,7 +3,6 @@ package child;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.content.fs.config.EnableFilesystemStores;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -11,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackageClasses = FileRepository.class)
 @EnableFilesystemStores(basePackageClasses = FileContentStore.class)
 @EntityScan(basePackageClasses = File.class)
-class FileConfig {
+public class FileConfig {
 
 
 }
--- parent/src/main/java/gettingstarted/SpringContentApplication.java
+++ parent/src/main/java/gettingstarted/SpringContentApplication.java
@@ -2,10 +2,19 @@ package gettingstarted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import child.FileConfig;
 
 @SpringBootApplication
 public class SpringContentApplication {
 
+	@Configuration
+	@Import(FileConfig.class)
+	public static class AppConfig {
+
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringContentApplication.class, args);
 	}
